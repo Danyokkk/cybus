8d97fa5ffb0339af40f6a706bb28dfbd71beab7f
Danylo <126004115+Danyokkk@users.noreply.github.com>
2026-01-13 09:09:23 +0200

    UI Polish: Circular buttons, Fixed Mobile Toggle, Light Map Style

--- bus-tracker/frontend/components/Map.js
+++ bus-tracker/frontend/components/Map.js
@@ -424,7 +424,11 @@ export default function BusMap({ stops, shapes, routes, onSelectRoute, routeColo
                         <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}" />
                     </>
                 ) : (
-                    <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" />
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+                        maxZoom={20}
+                    />
                 )}
 
                 {shapes && shapes.length > 0 && shapes.map((shape, index) => (
