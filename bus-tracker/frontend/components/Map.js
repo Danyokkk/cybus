f93c12673fda81f686c6bde5d5af27c401cc1be8
Danyokkk <126004115+Danyokkk@users.noreply.github.com>
2026-03-03 01:43:48 +0200

    style: even wider horizontal map bounds

--- bus-tracker/frontend/components/Map.js
+++ bus-tracker/frontend/components/Map.js
@@ -568,7 +568,7 @@ export default function BusMap({
                 center={[35.1264, 33.4299]}
                 zoom={9}
                 minZoom={8}
-                maxBounds={[[32.5, 31.0], [36.5, 36.0]]}
+                maxBounds={[[32.5, 30.0], [36.5, 37.0]]}
                 maxBoundsViscosity={1.0}
                 style={{ height: '100%', width: '100%' }}
                 zoomControl={false}
